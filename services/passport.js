61702de192ec6c618d611121530903b070817493
dkang417 <dkang417@gmail.com>
2018-09-23 08:48:13 -0400

    added models routes and sevices

---
+++ services/passport.js
@@ -0,0 +1,33 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+// creates a new instance of google strategy- console.developers.google.com
+passport.use
+    (new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({ googleId: profile.id })
+                .then((existingUser) => {
+                    if (existingUser) {
+                        // we already have a record with the given profile id
+                        done(null, existingUser);
+                    } else {
+                        // we dont have a user record wit this id, make a new record
+                        new User({ googleId: profile.id })
+                            .save()
+                            .then(user => done(null, user));
+                    }
+                });
+        }
+    )
+);
+
+
