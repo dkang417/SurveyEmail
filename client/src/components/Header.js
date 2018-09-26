5dc42b49c1401bfb296cd5cf3becb4580f79d151
dkang417 <dkang417@gmail.com>
2018-09-26 09:37:20 -0400

    added logout features inside header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,11 +8,12 @@ class Header extends Component {
     renderContent() {
         switch (this.props.auth) {
             case null:
-                return 'Still deciding';
+                return;
             case false:
-                return 'im logged out';
+                return <li><a href="/auth/google">Login With google</a></li>;
+                
             default:
-                return 'im logged in';    
+                return <li><a href="/api/logout">Logout</a></li>;    
         }
     }
     render() {
