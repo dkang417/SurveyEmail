dbfd40a09c7bc345ed436d1bc6d886dfd6498f82
dkang417 <dkang417@gmail.com>
2018-09-26 08:30:54 -0400

    updated auth reducer to check for logged in user and update header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 
+// connecting authreducer to verify user in header
+import { connect } from 'react-redux';
+
 class Header extends Component {
+    // checks if user is logged in
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return 'Still deciding';
+            case false:
+                return 'im logged out';
+            default:
+                return 'im logged in';    
+        }
+    }
     render() {
+        console.log(this.props);
         return (
             <nav>
                 <div className="nav-wrapper">
                     <a className="left brand-logo">
                         Emaily    
                     </a>
+
                     <ul className="right">
-                        <li>
-                            <a>Login With Google</a>
-                        </li>
+                       {this.renderContent()}
                     </ul>
+
                 </div>
             </nav>
         );
     }
 }
-export default Header;
+// checking state of authreducer
+function mapStateToProps({auth}) {
+    return {auth};
+}
+export default connect(mapStateToProps)(Header);
