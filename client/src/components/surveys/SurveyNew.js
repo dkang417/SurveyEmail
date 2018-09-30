df388dbec92961aecebac7d8460990f26983e91b
dkang417 <dkang417@gmail.com>
2018-09-29 22:42:59 -0400

    set up form

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,15 @@
+// surveynew shows surveyform and surveyform review
+import React, { Component } from 'react';
+import SurveyForm from './SurveyForm';
+
+
+class SurveyNew extends Component {
+    render() {
+        return (
+            <div>
+                <SurveyForm />
+            </div>
+        );
+   }
+}
+export default SurveyNew;
