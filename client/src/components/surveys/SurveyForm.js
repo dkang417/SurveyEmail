ea31c3d3122af34258f6c888851a4fbe00ec8c4b
dkang417 <dkang417@gmail.com>
2018-10-01 19:50:00 -0400

    redirect on submit form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -6,17 +6,12 @@ import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 import { Link } from 'react-router-dom';
 import validateEmails from '../../utils/validateEmails';
+import formFields from './formFields';
 
-const FIELDS = [
-    {label: 'Survey Title', name: 'title'},
-    {label:'Subject Line', name: 'subject'},
-    {label: 'Email Body', name: 'body'},
-    {label: 'Recipient List', name: 'emails'}
-];
 
 class SurveyForm extends Component {
     renderFields() {
-        return _.map(FIELDS, ({label, name}) => {
+        return _.map(formFields, ({label, name}) => {
             return (
                 <Field key={name} component={SurveyField} type="text" label={label} name={name} />
             );
@@ -39,7 +34,7 @@ class SurveyForm extends Component {
 }
 function validate(values) {
     const errors = {};
-    errors.emails = validateEmails(values.emails || '');
+    errors.recipients = validateEmails(values.recipients || '');
     // if (!values.title) {
     //     errors.title = 'You must provide a title';
     // }
@@ -50,7 +45,7 @@ function validate(values) {
     //     errors.body = 'You must provide a body';
     // }
     // dry up above code here:
-    _.each(FIELDS, ({ name }) => {
+    _.each(formFields, ({ name }) => {
         if (!values[name]) {
             errors[name] = 'You must provide a value';
         }
@@ -62,5 +57,6 @@ function validate(values) {
 }
 export default reduxForm({
     validate,
-    form: 'surveyForm'
+    form:'surveyForm',
+    destroyOnUnmount: false
 })(SurveyForm);
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
+import { reduxForm } from 'redux-form';
+
 
 class SurveyNew extends Component {
     // constructor(props) {
@@ -28,4 +30,6 @@ class SurveyNew extends Component {
         );
    }
 }
-export default SurveyNew;
+export default reduxForm({
+    form: 'surveyForm'
+}) (SurveyNew);
