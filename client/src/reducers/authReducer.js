63dfcd868637dc76fcd6ef8f91baebca203b5d9a
dkang417 <dkang417@gmail.com>
2018-10-01 11:29:02 -0400

    remove console logs

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,7 @@
 import { FETCH_USER } from '../actions/types';
 // updates value and creates new piece of state and updates in the redux components
 export default function (state = null, action) {
-    console.log(action);
+    // console.log(action);
     switch (action.type) {
         case FETCH_USER:
             return action.payload || false;    
